819dad8531d8cabe537f85d6cc28ce674a162542
TiagoDoria <tiagodoriap@gmail.com>
2020-06-14 22:33:21 -0300

    adicionando classe motorista

---
+++ src/app/Motorista.ts
@@ -0,0 +1,11 @@
+export class Motorista {
+    nome: string;
+    email: string;
+    senha: string;
+    faixa_etaria: string;
+    carga_horaria: number;
+    alocacao: string;
+    status: Boolean;
+    data_criacao: Date
+
+}
